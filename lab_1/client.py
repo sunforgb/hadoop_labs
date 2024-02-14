3ea4405dd6606186070fe02903a745694e7bdf39
= <=>
2024-02-14 21:23:37 +0300

    add ls, lls, cd

--- lab_1/client.py
+++ lab_1/client.py
@@ -1,22 +1,60 @@
 import os
 import requests
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 import argparse
 import cmd
 import sys
-
-usage = """
-    This is future help message
-"""
-hadoop_url = "http://192.168.122.8:50070/webhdfs/v1/"
+from lib import HDFSObject, LocalFSObject
+import stat
 
 
 class HDFSClient(cmd.Cmd):
 
     def __init__(self, base_url: str, base_port: str, user: str):
         cmd.Cmd.__init__(self)
-        self.url = f"http://{base_url}:{base_port}/webhdfs/v1/"
+        # save base_url as a root path
+        self.base_url = f"http://{base_url}:{base_port}/webhdfs/v1/"
+        # context path
+        self.pwd = f"http://{base_url}:{base_port}/webhdfs/v1/"
         self.request_params = {"user.name": user}
+
+    def __fix_dots(self, path: str, base: list=[]) -> list:
+        for part in path.split("/"):
+            if not part or part == ".":
+                continue
+            if base and part == "..":
+                base.pop()
+            else:
+                base.append(part)
+        return base
+
+
+    def _fix_path(self, path: str):
+        # absolute paths, just make it
+
+        if path.startswith("/"):
+            target_path = self.base_url
+            path = path.strip("/")
+            return urljoin(target_path, path)
+        #
+        path = "" if path is None else path.strip()
+        # prepare mini-stack for dots
+        spl = []
+        tmp_1 = urlparse(self.base_url).path.split("/")
+        tmp_2 = urlparse(self.pwd).path.split("/")
+        for item in tmp_2:
+            if item not in tmp_1:
+                spl.append(item)
+        spl = self.__fix_dots(path, spl)
+        request_url = urljoin(self.base_url, "/".join(spl))
+        return request_url
+    
+    def _fix_local_path(self, path: str):
+        path = "" if path is None else path.strip()
+        if not path.startswith("/"):
+            path = f"{os.getcwd()}/{path}"
+        spl = self.__fix_dots(path)
+        return "/"+"/".join(spl)
 
     def do_exit(self, _):
         """
@@ -38,35 +76,41 @@ class HDFSClient(cmd.Cmd):
          Creates remote directory
         """
         request_param = {"op": "MKDIRS"}
-        response = requests.put(urljoin(self.url, remote_path), params=request_param)
+        response = requests.put(self._fix_path(remote_path), params=request_param)
         if response.status_code == 200:
-            return response.json()["boolean"]
+            print("directory was created!")
         else:
             return False
 
-    def do_put(self, data: str, path: str) -> bool:
+    def do_put(self, input_line: str) -> bool:
         """
         Usage: put <local file> <remote_dir>
 
         Upload local file into remote directory
         """
+        local_path, remote_path = input_line.split()
         request_param = {"op": "CREATE", "overwrite": True}
         response = requests.put(
-            urljoin(self.url, path), params=request_param, allow_redirects=False
+            self._fix_path(remote_path), params=request_param, allow_redirects=False
         )
-        # TODO check for redirect status code
+        if response.status_code != 307:
+            print("Error in create_file request, status_code: ", response.status_code)
+            return False
         load_headers = {"content-type": "application/octet-stream"}
-        with open(data, "rb") as file:
+        with open(local_path, "rb") as file:
             content = file.read()
             response = requests.put(
                 response.headers["location"],
                 headers=load_headers,
-                files={data: content},
+                files={local_path: content},
             )
-        print(response.status_code)
-        # TODO check for 201 status_code
+            if response.status_code == 201:
+                print("File was created")
+            else:
+                print("Error in create_file request, status_code: ", response.status_code)
+        
 
-    def do_get(self, path: str) -> bytes:
+    def do_get(self, remote_path: str) -> bytes:
         """
         Usage: get <remote_file>
 
@@ -74,78 +118,133 @@ class HDFSClient(cmd.Cmd):
         """
         request_param = {"op": "OPEN"}
         response = requests.get(
-            urljoin(self.url, path), params=request_param, allow_redirects=True
+            self._fix_path(remote_path), params=request_param, allow_redirects=True
         )
-        return response.content
+        if response.status_code != 200:
+            print("Error in get_file, status_code:", response.status_code)
+            return False
+        local_path = remote_path.split("/")[-1]
+        with open(local_path, "wb") as f:
+            f.write(response.content)
 
-    def do_append(self, file_path: str, path: str):
+    def do_append(self, input_line: str):
         """
         Usage: append <local_file> <remote_file>
 
         Append local file to remote file
         """
+        local_path, remote_path = input_line.split(" ")
         request_param = {"op": "APPEND"}
         response = requests.post(
-            urljoin(self.url, path), params=request_param, allow_redirects=False
+            self._fix_path(remote_path), params=request_param, allow_redirects=False
         )
+        if response.status_code != 307:
+            print("Error in append_file request, status_code: ", response.status_code)
+            return False
         load_headers = {"content-type": "application/octet-stream"}
-        with open(file_path, "rb") as f:
+        with open(local_path, "rb") as f:
             content = f.read()
         response = requests.post(
             response.headers["location"],
             headers=load_headers,
-            files={file_path: content},
+            files={local_path: content},
         )
-        print(response.status_code)
+        if response.status_code == 200:
+            print("File was appended")
+        else:
+            print("Error in append_file request, status_code: ", response.status_code)
+            return False
 
-    def do_delete(self, path: str):
+    def do_delete(self, remote_path: str):
         """
         Usage: delete <remote_file>
 
         Delete remote file
         """
         request_param = {"op": "DELETE"}
-        response = requests.delete(urljoin(self.url, path), params=request_param)
+        response = requests.delete(self._fix_path(remote_path), params=request_param)
         if response.status_code == 200:
-            return response.json()["boolean"]
+            print("Remote file was deleted")
         else:
+            print("Error in delete_file request, status_code: ", response.status_code)
             return False
+    def _complete_ls(self, objects: list[HDFSObject|LocalFSObject]):
+        columns = ["mode", "repl", "owner", "group", "size", "name"]
+        lengths = dict(zip(columns, [0] * len(columns)))
+        build = {}
+        align = {
+            'repl': '>',
+            'size': '>',
+        }
+        objs = []
+        for item in objects:
+            tmp = {}
+            for name in columns:
+                text = build.get(name, "{}").format(getattr(item, name))
+                tmp[name] = text
+                lengths[name] = max(lengths[name], len(text))
+            objs.append(tmp)
+            text = text = ' '.join('{%s:%s%s}' % (i, align.get(i, ''), lengths[i]) for i in columns)
+        for item in objs:
+            print(text.format(**item))
 
-    # TODO check for statuses and pretty print
-    def do_ls(self, path: str = "."):
+    def do_ls(self, remote_path: str = "."):
         """
         Usage: ls [remote_dir]
 
         List remote directory. If remote dir not provided, then list current dir.
         """
+        objects = []
         request_param = {"op": "LISTSTATUS"}
-        response = requests.get(urljoin(self.url, path), params=request_param)
-        for item in response.json()["FileStatuses"]["FileStatus"]:
-            print(item["pathSuffix"])
+        path = self._fix_path(remote_path)
+        response = requests.get(path, params=request_param)
+        if response.status_code == 200:
+            for item in response.json()["FileStatuses"]["FileStatus"]:
+                objects.append(HDFSObject(path, item))
+            self._complete_ls(objects)
+        elif response.status_code == 404:
+            print("No such file or directory")
+        else:
+            print("Error in list_files, status_code: ", response.status_code)
+            return False
 
-    def do_cd(self):
+    def do_cd(self, remote_path: str):
         """
         Usage: cd <remote_dir>
 
         Change current work directory in HDFS.
         """
-        pass
+        self.pwd = self._fix_path(remote_path)
 
-    def do_lls(self):
+    def do_lls(self, local_path: str):
         """
         Usage: lls [local_dir]
 
         List local directory. If local directory not provided, then list current dir
         """
-        pass
+        objects = []
+        try:
+            path = self._fix_local_path(local_path)
+            info = os.stat(path)
+            if stat.S_ISDIR(info.st_mode):
+                objects = list(LocalFSObject(path, name) for name in os.listdir(path))
+            elif stat.S_ISREG(info.st_mode):
+                objects = [LocalFSObject(os.path.dirname(path), os.path.basename(path))]
+            self._complete_ls(objects)
+        except (KeyError, OSError) as e:
+            print("Smth went wrong in local ls")
+            print(e)
 
-    def do_lcd(self):
+        
+        
+
+    def do_lcd(self, local_path: str):
         """
         Usage: lcd <local_dir>
 
         Change current local working directory.
         """
-        pass
+        os.chdir(local_path)
 
     def do_doc(self, line):
         help_message = """
@@ -178,11 +277,12 @@ if __name__ == "__main__":
         description="webhdfs client shell",
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
-    parser.add_argument("-u", "--url", help="WebHDFS URL")
+    parser.add_argument("-d", "--url", help="WebHDFS URL")
     parser.add_argument("-p", "--port", help="WebHDFS port")
-    parser.add_argument("-U", "--user", help="WebHDFS username")
+    parser.add_argument("-u", "--user", help="WebHDFS username")
     args = parser.parse_args()
     try:
+        print(args)
         main(args.url, args.port, args.user)
     except KeyboardInterrupt:
         print("Good Bye")
---
+++ lab_1/lib.py
@@ -0,0 +1,141 @@
+import os
+import stat
+
+#Just filemode table from stat
+_permissions = (
+    ((stat.S_IFREG,              "-"),
+     (stat.S_IFDIR,              "d")),
+
+    ((stat.S_IRUSR,              "r"),),
+    ((stat.S_IWUSR,              "w"),),
+    ((stat.S_IXUSR,              "x"),),
+
+    ((stat.S_IRGRP,              "r"),),
+    ((stat.S_IWGRP,              "w"),),
+    ((stat.S_IXGRP,              "x"),),
+
+    ((stat.S_IROTH,              "r"),),
+    ((stat.S_IWOTH,              "w"),),
+    ((stat.S_IXOTH,              "x"),)
+)
+
+
+def perm_to_mode(perm: str):
+    mode = []
+    for t in _permissions:
+        for b, c in t:
+            if perm & b == b:
+                mode.append(c)
+                break
+            else:
+                mode.append("-")
+    return ''.join(mode)
+
+def fix_encoding(bits: str|bytes):
+    return str(bits, "utf-8") if isinstance(bits, bytes) else bits
+
+class HDFSObject():
+    def __init__(self, path:str, bits):
+        self.path = path.rstrip("/")
+        self.bits = bits
+
+        if not self.bits["pathSuffix"]:
+            self.bits["pathSuffix"] = os.path.basename(self.path)
+            self.path = os.path.dirname(fix_encoding(self.path)).rstrip("/")
+        
+        self.info = {
+            "perm": int(self.bits["permission"], 8) | (stat.S_IFDIR if self.is_dir() else stat.S_IFREG)
+        }
+        self.info["mode"] = perm_to_mode(self.info["perm"])
+    
+    def __getstate__(self) -> object:
+        return {"path": self.path, "bits": self.bits}
+    
+    def __setstate__(self,args):
+        self.__init__(**args)
+    
+    def __str__(self):
+        return self.full_name
+    
+    def is_dir(self):
+        return self.type == "DIRECTORY"
+    
+    def is_file(self):
+        return self.type == "FILE"
+    
+    @property
+    def owner(self):
+        return fix_encoding(self.bits["owner"])
+    
+    @property
+    def group(self):
+        return fix_encoding(self.bits["group"])
+    
+    @property
+    def name(self):
+        return fix_encoding(self.bits['pathSuffix'])
+    @property
+    def full_name(self):
+        return f"{self.path}/{self.name}"
+    @property
+    def size(self):
+        return self.bits["length"]
+    @property
+    def repl(self):
+        return self.bits["replication"]
+    @property
+    def type(self):
+        return fix_encoding(self.bits["type"])
+    @property
+    def mode(self):
+        return self.info["mode"]
+    @property
+    def perm(self):
+        return self.info["perm"]
+    
+class LocalFSObject():
+    def __init__(self, path, name):
+        self.path = path.rstrip("/")
+        self.bits = os.stat(f"{self.path}/{name}")
+        self.info = {
+            "name": name,
+            "type": "DIRECTORY" if stat.S_ISDIR(self.bits.st_mode) else "FILE",
+            "mode": perm_to_mode(self.bits.st_mode)
+        }
+        self.info["owner"] = str(self.bits.st_uid)
+        self.info['group'] = str(self.bits.st_gid)
+
+    def __str__(self):
+        return self.full_name
+    
+    def is_dir(self):
+        return self.type == "DIRECTORY"
+    
+    @property
+    def owner(self):
+        return self.info["owner"]
+    @property
+    def group(self):
+        return self.info["group"]
+    @property
+    def name(self):
+        return self.info["name"]
+    @property
+    def full_name(self):
+        return f"{self.path}/{self.name}"
+    @property
+    def size(self):
+        return self.bits.st_size
+    @property
+    def repl(self):
+        return self.bits.st_nlink
+    @property
+    def type(self):
+        return self.info["type"]
+    @property
+    def mode(self):
+        return self.info["mode"]
+    @property
+    def perm(self):
+        return self.bits.st_mode
+    
